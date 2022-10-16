914e6f0db0c1ea767bfda9b51be063126b0b2aa2
thirstyturtle125 <114880688+thirstyturtle125@users.noreply.github.com>
2022-10-16 15:34:48 -0400

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+
+(function() {
+  "use strict";
+
+  /**
+   *  Selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+
+})()
